bcb582283867b0386bae2b05f563f7a6bab7d420
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-10-12 22:08:21 +0900

    re

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 export const config = {
   chainId: 56,
   contractAddressUSDT: "0x81eD26229b6D38C215fBe833cE277E35392D3604",
-  contractAddress: "0xa1138543062c4CB0Bd3C2057752db2773F261235",
+  contractAddress: "0x061ACB3e73B9e0f7e550E9F9119483faEB2d4A4F",
   lottoryContractAddress: "0xF07049221B3CdA5C34e0e470B9709B690Fd28C9B", // "0x6b011A9A370fC0F7238b69Da0212463d51d96aAc",
   scanLink:  // "https://testnet.bscscan.com/address/0x2fe004ae1b6718b09380f8392aa91cd2d5039b98#code",
     "https://bscscan.com/address/0x2fe004ae1b6718b09380f8392aa91cd2d5039b98",
