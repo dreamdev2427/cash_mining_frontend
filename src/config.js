110786c125f786641de0c095a2a27f5ee3cca8e3
CrystalBlockDev <crytal.blockdev@gmail.com>
2022-10-12 21:53:10 +0900

    update contract address

--- src/config.js
+++ src/config.js
@@ -1,8 +1,12 @@
 export const config = {
   chainId: 56,
   contractAddressUSDT: "0x81eD26229b6D38C215fBe833cE277E35392D3604",
-  contractAddress: "0x1700DB905DB229903c9Dd50aA3F4EBaF4d1eD181",
+  contractAddress: "0xa1138543062c4CB0Bd3C2057752db2773F261235",
   lottoryContractAddress: "0xF07049221B3CdA5C34e0e470B9709B690Fd28C9B", // "0x6b011A9A370fC0F7238b69Da0212463d51d96aAc",
   scanLink:  // "https://testnet.bscscan.com/address/0x2fe004ae1b6718b09380f8392aa91cd2d5039b98#code",
     "https://bscscan.com/address/0x2fe004ae1b6718b09380f8392aa91cd2d5039b98",
 };
+
+
+//owner 0xc384e872ac48b0B592314584df0E6eFA2536b422
+//dev 0xe96c26520900297fBE95eab592e977F4e98Ffc4C
